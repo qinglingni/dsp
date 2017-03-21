cd4d7437887759ff4fc3bf8d6da0120ea895cf8c
Qingling <qingling.ni@gmail.com>
2017-03-20 21:56:00 -0700

    Update q5_datetime.py
    Code added

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,7 +1,7 @@
 # Hint:  use Google to find python function
 
 ####a) 
-date_start = '01-02-2013'  
+"""date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
 ####b)  
@@ -10,4 +10,10 @@ date_stop = '05282015'
 
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'  """
+
+date1 = datetime.strptime(date_stop,'%d-%b-%Y')
+date2 = datetime.strptime(date_start,'%d-%b-%Y')
+
+delta = date1-date2
+print (delta)
